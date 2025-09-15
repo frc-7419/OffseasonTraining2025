a508bcc5108d8503d309f9ce91080d312d4d93fe
Sepandar <sepandar24@gmail.com>
2025-09-15 15:07:14 -0700

    arm subsystem

---
+++ src/main/java/frc/robot/constants/Constants.java
@@ -0,0 +1,74 @@
+package frc.robot.constants;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import edu.wpi.first.units.measure.*;
+
+public class Constants {
+  public static class RobotConstants {
+    public static final String kCANivoreBus = "7419";
+  }
+
+  public static class ArmConstants {
+    public static final int kArmMotorID = 0; // Arbitrary ID (change)
+    public static final int kArmEncoderID = 0; // Arbitrary ID (change)
+    public static final AngularVelocity kMaxSpeed = RotationsPerSecond.of(1); // Arbitrary velocity (change)
+    public static final Angle kAngleTolerance = Degrees.of(5); // Arbitrary angle (change)
+    public static final Angle kMaxAngle = Degrees.of(90); // Arbitrary angle (change)
+    public static final Angle kMinAngle = Degrees.of(90); // Arbitrary angle (change)
+    public static final TalonFXConfiguration kArmTalonFXConfiguration = new TalonFXConfiguration();
+
+    static {
+      kArmTalonFXConfiguration.Feedback.FeedbackRemoteSensorID = kArmEncoderID;
+      kArmTalonFXConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+      kArmTalonFXConfiguration.Feedback.SensorToMechanismRatio = 1.0;
+      kArmTalonFXConfiguration.Feedback.RotorToSensorRatio = 1; // Don't know yet
+    }
+
+    public static final CANcoderConfiguration kArmCANCoderConfig = new CANcoderConfiguration();
+
+    static {
+      kArmCANCoderConfig.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+      kArmCANCoderConfig.MagnetSensor.withMagnetOffset(Rotations.of(0)); // Change offset
+    }
+
+    public static final Slot0Configs kArmSlot0Configs = kArmTalonFXConfiguration.Slot0;
+
+    static {
+      kArmSlot0Configs.kG = 0; // output to overcome gravity (output)
+      kArmSlot0Configs.kS = 0; // output to overcome static friction (output)
+      kArmSlot0Configs.kV = 0; // output per unit of target velocity (output/rps)
+      kArmSlot0Configs.kA = 0; // output per unit of target acceleration (output/(rps/s))
+      kArmSlot0Configs.kP = 0; // output per unit of error in position (output)
+      kArmSlot0Configs.kI = 0; // output per unit of integrated error in position (output)
+      kArmSlot0Configs.kD = 0; // output per unit of error in velocity (output/rps)
+    }
+
+    // https://v6.docs.ctr-electronics.com/en/latest/docs/api-reference/device-specific/talonfx/motion-magic.html#motion-magic-expo
+    public static final MotionMagicConfigs kMotionMagicConfig = kArmTalonFXConfiguration.MotionMagic;
+
+    static {
+      kMotionMagicConfig.MotionMagicCruiseVelocity = 0; // peak velocity of the profile; set to 0 to target the
+      // system’s max velocity
+      kMotionMagicConfig.MotionMagicExpo_kV = 0; // voltage required to maintain a given velocity, in V/rps
+      kMotionMagicConfig.MotionMagicExpo_kA = 0; // voltage required to maintain a given velocity, in V/rps
+    }
+
+    public static final CurrentLimitsConfigs kCurrentLimitConfig = kArmTalonFXConfiguration.CurrentLimits;
+
+    static {
+      kCurrentLimitConfig.StatorCurrentLimit = 80; // current limit in amps
+      kCurrentLimitConfig.StatorCurrentLimitEnable = true; // enable current limiting
+    }
+
+    public static final AngularVelocity UNSAFE_SPEED = RotationsPerSecond.of(1); // 1 rad/s
+    public static final Temperature MAX_TEMPERATURE = Celsius.of(90); // Max rated temperature
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,140 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Celsius;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.constants.Constants.ArmConstants;
+
+/**
+ * The {@code ArmSubsystem} class controls the wrist subsystem of the robot.
+ * It manages the motion
+ * and angle of the elevator using TalonFX motors with a fused CANCoder.
+ */
+public class ArmSubsystem extends SubsystemBase {
+  private enum ControlMode {
+    MANUAL,
+    MOTIONMAGIC
+  }
+
+  private final TalonFX wristMotor = new TalonFX(ArmConstants.kArmMotorID);
+
+  private final CANcoder wristEncoder = new CANcoder(ArmConstants.kArmEncoderID);
+  private final VelocityVoltage velocityRequest = new VelocityVoltage(0).withSlot(0);
+
+  private final MotionMagicExpoVoltage motionMagicRequest = new MotionMagicExpoVoltage(0).withSlot(0);
+
+  private ControlMode controlMode = ControlMode.MANUAL;
+
+  /** Creates a new {@code ArmSubsystem} with a TalonFX and a CANcoder. */
+  public ArmSubsystem() {
+    wristMotor.getConfigurator().apply(ArmConstants.kArmTalonFXConfiguration);
+    wristEncoder.getConfigurator().apply(ArmConstants.kArmCANCoderConfig);
+    brake();
+  }
+
+  /**
+   * Sets the wrist power in manual mode.
+   *
+   * @param power The power to set, ranging from -1 to 1. Positive values move the
+   *              elevator up, and
+   *              negative values move it down.
+   */
+  public void setPower(double power) {
+    if (controlMode == ControlMode.MOTIONMAGIC) {
+      return;
+    }
+
+    power = Math.max(-1, Math.min(1, power));
+
+    wristMotor.setControl(
+        velocityRequest
+            .withVelocity(power * ArmConstants.kMaxSpeed.in(RotationsPerSecond))
+            .withLimitForwardMotion(getPosition().gte(ArmConstants.kMaxAngle))
+            .withLimitReverseMotion(getPosition().lte(ArmConstants.kMinAngle)));
+  }
+
+  /**
+   * Returns a Command that drives the wrist to a specific angle and then ends,
+   * returning the
+   * control mode to MANUAL.
+   *
+   * @param angle The target angle
+   * @return A command for scheduling.
+   */
+  public Command setAngle(Angle angle) {
+    return this.runEnd(() -> toAngle(angle), () -> switchControlMode(ControlMode.MANUAL));
+  }
+
+  /**
+   * Returns a Command that applies manual wrist control (ex, from a joystick).
+   *
+   * @param power The power from -1 to 1.
+   * @return A command for scheduling.
+   */
+  public Command joystickControl(CommandXboxController joystick) {
+    return this.run(() -> setPower(joystick.getRightY()));
+  }
+
+  /** Sets the TalonFX to brake mode (resists motion when no power is applied). */
+  public void brake() {
+    wristMotor.set(0);
+    wristMotor.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+  /** Sets the TalonFX to coast mode (spins freely when no power is applied). */
+  public void coast() {
+    wristMotor.setNeutralMode(NeutralModeValue.Coast);
+  }
+
+  public Angle getPosition() {
+    return wristEncoder.getAbsolutePosition().getValue();
+  }
+
+  public AngularVelocity getVelocity() {
+    return wristEncoder.getVelocity().getValue();
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Arm Angle (Rotations)", getPosition().in(Rotations));
+    SmartDashboard.putNumber("Arm Angle (Degrees)", getPosition().in(Degrees));
+    SmartDashboard.putNumber(
+        "Arm Velocity (RotationsPerSecond)", getVelocity().in(RotationsPerSecond));
+    SmartDashboard.putNumber(
+        "Arm Temperature (Celsius)", wristMotor.getDeviceTemp().getValue().in(Celsius));
+  }
+
+  /**
+   * Moves the wrist to a specified angle using Motion Magic.
+   *
+   * @param angle The desired angle (e.g., 0° is horizontal, 90° is vertical).
+   */
+  private void toAngle(Angle angle) {
+    controlMode = ControlMode.MOTIONMAGIC;
+
+    wristMotor.setControl(
+        motionMagicRequest
+            .withPosition(angle.in(Rotations))
+            .withLimitForwardMotion(getPosition().gte(ArmConstants.kMaxAngle))
+            .withLimitReverseMotion(getPosition().lte(ArmConstants.kMinAngle)));
+  }
+
+  /** Switches the current control mode. */
+  private void switchControlMode(ControlMode mode) {
+    this.controlMode = mode;
+  }
+}
